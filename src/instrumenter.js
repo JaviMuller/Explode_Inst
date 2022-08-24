5da45caf880a8100470e2f973f8406fdf4ac577f
JaviMuller <javier.de.muller@tecnico.ulisboa.pt>
2022-08-24 16:24:48 +0100

    Added optimization

--- src/instrumenter.js
+++ src/instrumenter.js
@@ -11,22 +11,34 @@
 const yargs = require("yargs");
 const fs = require("fs");
 const utils = require("../utils/js_ast_generation/ast_utils");
-const test_gen = require("./test_gen")
+const test_gen = require("./test_gen");
+const optim = require("./optim");
 
 /**
  * Command line interface
  */
 const argv = yargs
-	.option("input", { alias: "i", description: "JS input file", type: "string" })
-	.option("config", { alias: "c", description: "JSON specification of sinks, source function and variable types", type: "string" })
-	.option("optim", { alias: "opt", description: "JSON specification of computation path", type: "string" })
+	.option("input", {
+		alias: "i",
+		description: "JS input file",
+		demandOption: true,
+		type: "string"
+	})
+	.option("config", {
+		alias: "c",
+		description: "JSON specification of sinks, source function and variable types",
+		demandOption: true,
+		type: "string"
+	})
+	.option("optim", {
+		alias: "opt",
+		description: "JSON specification of computation path"
+	})
 	.option("output", {
 		alias: "o",
 		description: "ECMA-SL output file",
 		type: "string",
 	})
-	.demandOption("input")
-	.demandOption("config")
 	.usage("Usage: $0 -i [filepath]")
 	.help()
 	.alias("help", "h").argv;
@@ -53,8 +65,8 @@ try {
 /* TODO (Optimization) -> Remove all pieces of code not executed in the assert/
  * console.log computation path
 */
-if (argv.optim) {
-	ast = test_gen.remove_unused(ast, optim);
+if (argv.optim && config.lines) {
+	ast = optim(ast, config.lines);
 }
 
 /******************      Step 3 - Generate specific test      *****************/
--- src/test_gen.js
+++ src/test_gen.js
@@ -3,7 +3,6 @@
  * config file
  */
 
-
 /**
  * Require
  */
